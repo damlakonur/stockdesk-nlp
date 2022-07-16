9f1c3d2b291b94b5d637bff08321dfa0c2da32e6
Serife Damla Konur <konurs16@itu.edu.tr>
2022-07-17 01:19:22 +0300

    kod temizleme

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,18 +1,10 @@
 import React from 'react'
 
-const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
-
-// Cyberdroid Routes
-
-const UebaPage = React.lazy(() => import('./views/ueba/UebaPage'))
-
+const InfluencerPage = React.lazy(() => import('./views/Influencer/InfluencerPage'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
-  { path: '/dashboard', name: 'Dashboard', element: Dashboard },
-  { path: '/analysis/ueba', name: 'UEBA', element: UebaPage },
-
-
+  { path: '/analysis/Influencer', name: 'Influencer', element: InfluencerPage },
 ]
 
 export default routes
