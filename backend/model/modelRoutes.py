4c2aae5a6ed722b4e868a439b60a87b7dea72827
enescanguven <enescanguven@gmail.com>
2022-08-12 23:58:44 +0300

    zart zurt

--- backend/model/modelRoutes.py
+++ backend/model/modelRoutes.py
@@ -6,7 +6,18 @@ import yfinance as yf
 
 model_bp = Blueprint('model_bp', __name__, template_folder='templates')
 
-@stock_bp.route('/model', methods=["GET"])
+#import modelleri
+
+
+@model_bp.route('/model', methods=["POST"])
 def get_prediction():
     try:
-        
+        tweet = request.get_json()['tweet']
+        # pipline
+        # model
+        # prediction
+        # return prediction
+
+        return {"prediction": "success"}, 200
+    except Exception as e:
+        return json.dumps({"error": str(e)})
